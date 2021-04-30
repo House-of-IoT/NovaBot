51b4b3d5e6a00a1cbf702b3ff5e3ec47f2e8a8b0
RonaldColyar <phantexcreations@gmail.com>
2021-04-30 19:43:37 -0400

    router init

---
+++ Speech/Router.py
@@ -0,0 +1,14 @@
+class Router:
+    def __init__(self , parent):
+        self.parent = parent
+    
+
+    def route_priority_one(self ,command):
+       
+        if command == "wikipedia":
+            pass
+                        
+            
+
+            
+        
