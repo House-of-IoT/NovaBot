720c0ec014a0d8b45c3857b86e63aa8bde6f3b47
RonaldColyar <phantexcreations@gmail.com>
2021-04-30 20:50:33 -0400

    init functionality

--- Speech/Router.py
+++ Speech/Router.py
@@ -7,7 +7,25 @@ class Router:
        
         if command == "wikipedia":
             pass
-                        
+        elif command == "status":
+            pass
+        elif command == "twitter status":
+            pass
+        elif command == "github status":
+            pass
+        elif command == "disable camera":
+            pass
+        elif command == "silent mode":
+            pass
+        elif command == "logging mode":
+            pass
+        elif command = "peer status":
+            pass
+        else:
+            pass
+
+
+
             
 
             
