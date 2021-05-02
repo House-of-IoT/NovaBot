9bb1cdb38f747e3060776b6732571ee113e48a1b
RonaldColyar <phantexcreations@gmail.com>
2021-05-02 02:45:33 -0400

    added logger

---
+++ CommandFunctionality/logger.py
@@ -0,0 +1,24 @@
+
+from datetime import date
+
+today = date.today()
+
+
+class Logger:
+    def __init__(self):
+        self.count = 0
+        self.errors = 0
+
+    def write_to_log_file(message):
+        with open("../logs.txt") as file:
+            file.writeline(message)
+
+    def error(self , message):
+        today = date.today()
+        new_message = str(today) +" error:" + message
+        self.write_to_log_file(new_message)
+    def start():
+        message = "Starting Nova.."
+        self.write_to_log_file(message)
+    
+    
--- CommandFunctionality/request_handler.py
+++ CommandFunctionality/request_handler.py
@@ -6,8 +6,10 @@ class RequestHandler:
         self.count = 0
         self.data = None
         self.parent = parent
+
     def issue():
         self.parent.say("There was an issue with the request!")
+        
     def get(self,url,timeout):
         try:
             data = requests.get("https://geek-quote-api.herokuapp.com/v1/quote/1" , timeout = timeout)
@@ -22,16 +24,26 @@ class RequestHandler:
             self.parent.say(self.data[0]["quote"])
         else:
             self.issue()
-            
-    
+        
     def say_weather_current(self,place):
         url  = f"http://api.weatherapi.com/v1/current.json?key=c74cbac3960c4c729e052715210205&q={place}&aqi=no"
         if self.get(url) == True:
+            temp_f = self.data["current"]["temp_f"]
+            temp_c = self.data["current"]["temp_c"]
+            wind_mph = self.data["wind_mph"]
             self.parent.say(f"Here is the data for {place}")
-            self.parent.say(f"{data["current"]["temp_f"]}  degrees fahrenheit")
-            self.parent.say(f"{data["current"]["temp_c"]}  degrees celcius")
-            self.parent.say(f" wind speed is {data["wind_mph"]} miles per hour")
+            self.parent.say(f"{temp_f}  degrees fahrenheit")
+            self.parent.say(f"{temp_c}  degrees celcius")
+            self.parent.say(f" wind speed is {wind_mph} miles per hour")
         else:
            self.issue()
+
+    def say_geek_joke(self):
+        if self.get("https://geek-jokes.sameerkumar.website/api",5)  == True:
+            self.parent.say(self.data[0]["quote"])
+        else:
+            self.issue()
+
+    
 
     
