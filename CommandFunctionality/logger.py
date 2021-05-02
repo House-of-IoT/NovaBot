97b819dc20bb6589cae301d979bc90ebca468df6
RonaldColyar <phantexcreations@gmail.com>
2021-05-02 04:26:21 -0400

    added preference

--- CommandFunctionality/logger.py
+++ CommandFunctionality/logger.py
@@ -10,14 +10,14 @@ class Logger:
         self.errors = 0
 
     def write_to_log_file(message):
-        with open("../logs.txt") as file:
+        with open("../logs.txt" , "r") as file:
             file.writeline(message)
 
     def error(self , message):
         today = date.today()
         new_message = str(today) +" error:" + message
         self.write_to_log_file(new_message)
-        
+
     def start():
         message = "Starting Nova.."
         self.write_to_log_file(message)
