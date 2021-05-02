bf456c92af044fd1a484f325b5125166e8e8459b
RonaldColyar <phantexcreations@gmail.com>
2021-05-01 23:55:00 -0400

    moving foward with geek api WIP

--- CommandFunctionality/wiki_handler.py
+++ CommandFunctionality/wiki_handler.py
@@ -3,9 +3,10 @@ import wikipedia
 
 
 class WikiHandler:
-    def __init__(self):
+    def __init__(self,parent):
         self.count = 0
         self.current_result = None
+        self.parent = parent
 
     def gather_page(self,page_name):
         self.count +=1
@@ -20,4 +21,13 @@ class WikiHandler:
         
     def get_attempts(self):
         self.count
+
+    def search_and_say(self, input_data):
+        page_name = input_data[6::]
+        print(page_name)
+        result = self.gather_page(page_name)
+        if result == True:
+            self.parent.say(self.current_result)
+        else:
+            self.parent.say(f"Don't know what {page_name} is")
             
