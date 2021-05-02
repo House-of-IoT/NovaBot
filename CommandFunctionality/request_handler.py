fcba9414506abd5850218ec38324967899d80b80
RonaldColyar <phantexcreations@gmail.com>
2021-05-02 01:52:53 -0400

    new weather access

--- CommandFunctionality/request_handler.py
+++ CommandFunctionality/request_handler.py
@@ -5,15 +5,33 @@ class RequestHandler:
     def __init__(self , parent):
         self.count = 0
         self.data = None
-
-    def gather_geek_quote(self):
+        self.parent = parent
+    def issue():
+        self.parent.say("There was an issue with the request!")
+    def get(self,url,timeout):
         try:
-            data = requests.get("https://geek-quote-api.herokuapp.com/v1/quote/1" , timeout = 2)
-            data_dict = json.loads(data.content)
-            self.data = data_dict["quote"]
+            data = requests.get("https://geek-quote-api.herokuapp.com/v1/quote/1" , timeout = timeout)
+            data_dict = json.loads(data.content.decode("utf8"))
+            self.data = data_dict
             return True
         except:
             return False
+
+    def say_geek_quote(self):
+        if self.get("https://geek-quote-api.herokuapp.com/v1/quote/1" , 5) == True:
+            self.parent.say(self.data[0]["quote"])
+        else:
+            self.issue()
+            
     
-       
- 
+    def say_weather_current(self,place):
+        url  = f"http://api.weatherapi.com/v1/current.json?key=c74cbac3960c4c729e052715210205&q={place}&aqi=no"
+        if self.get(url) == True:
+            self.parent.say(f"Here is the data for {place}")
+            self.parent.say(f"{data["current"]["temp_f"]}  degrees fahrenheit")
+            self.parent.say(f"{data["current"]["temp_c"]}  degrees celcius")
+            self.parent.say(f" wind speed is {data["wind_mph"]} miles per hour")
+        else:
+           self.issue()
+
+    
