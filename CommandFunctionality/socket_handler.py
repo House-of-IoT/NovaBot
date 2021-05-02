50fb7c3fb30afd853bc0bf479d5912d9ff51ec3d
RonaldColyar <phantexcreations@gmail.com>
2021-05-02 04:03:33 -0400

    added rat functionality for central server

--- CommandFunctionality/socket_handler.py
+++ CommandFunctionality/socket_handler.py
@@ -2,9 +2,10 @@ import socket
 import threading
 
 class SocketHandler:
-    def __init__(self):
+    def __init__(self , parent):
         self.connected = False
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.parent = parent
 
     def connect_to_master_pi(self):
         try:
@@ -13,11 +14,25 @@ class SocketHandler:
         except:
             self.speech.say("Can't connect to server , please try again")
             return False
-            
+
     def listen():
         message = self.client.recv(100).decode("utf8")
+        if message == "disable":
+            self.parent.enabled = False
+        elif message == "say":
+            phrase = self.client.recv(100).decode("utf8")
+            self.parent.speech.say(phrase)
+        elif message == "stream_video":
+            pass
+        elif message == "stream_audio":
+            pass
+        elif message == "alarm":
+            pass
+        elif message == "performance":
+            pass
         
     def start_listening_from_master():
         if self.connect_to_master_pi() == True:
-            thread = threading.Thread()
+            thread = threading.Thread(target= self.listen)
+            thread.start()
 
