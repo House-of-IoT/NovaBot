878c62b2a38d025193c339c942cb71fb42214be1
RonaldColyar <phantexcreations@gmail.com>
2021-05-02 03:42:03 -0400

    removed socket logic for better encapsulation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import CommandFunctionality.wiki_handler
+
 from Speech.Speech import Speech
 import socket
 
@@ -6,17 +6,9 @@ import socket
 
 class Main:
     def __init__(self):
-        self.speech = Speech(CommandFunctionality.wiki_handler)
+        self.speech = Speech()
         self.count = 0
         self.running = True
-        self.connected = False
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-    def connect_to_master_pi(self):
-        try:
-            self.client.connect(('127.0.0.1',50222))
-        except:
-            self.speech.say("Can't connect to server , please try again")
 
     def start(self):
         self.speech.say("You are not connected to the local server! Just a note.")
@@ -28,6 +20,6 @@ class Main:
 
 if __name__ == "__main__":
     main = Main()
-    main.start()
+    main.speech.request_handler.say_geek_joke()
         
         
