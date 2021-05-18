abf769d1840b45c431cf97acbcaede488a038793
Ronaldcolyar <phantexcreations@gmail.com>
2021-05-18 01:26:39 -0500

    minor

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from Speech.Speech import Speech
 from CommandFunctionality.socket_handler import SocketHandler
 from CommandFunctionality.camera_handler import CameraHandler
@@ -16,27 +15,26 @@ class Main:
 
     async def start(self:int) :
 
-            self.speech.say("Hello")
+            self.speech.say("Welcome Back")
             while True:
                 await asyncio.sleep(3)
+
                 if self.enabled == True:
                     await self.speech.gather_voice_init()
                 else:
                     if self.streaming == True:
-                        pass #stream 
+                        if self.camera_handler.setup_was_successful:
+                            self.camera_handler.stream_data(self.socket_handler.connection)
+                        else:
+                            self.enabled = True
                     else:
                         pass #do nothing
 
-
-
     async def listen(self):
-            
             t2 = loop.create_task(self.start())
             t1 = loop.create_task(self.socket_handler.listen())
             await asyncio.wait([t1,t2])
 
-
-    
 if __name__ == "__main__":  
   main = Main() 
   loop = asyncio.get_event_loop()
