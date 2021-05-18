e3ed10bcd92045fbfc9a9c38a19bf738df58baf0
Ronaldcolyar <phantexcreations@gmail.com>
2021-05-18 00:04:09 -0500

    progression

--- CommandFunctionality/camera_handler.py
+++ CommandFunctionality/camera_handler.py
@@ -1,17 +1,30 @@
 import io
-import socket
-import struct
 import time
 import picamera
+import asyncio
 
 class CameraHandler:
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
+        self.setup_was_successful = True
         try:
-            self.camera = picamera.PiCamera()
+            self.camera = picamera.PiCamera(framerate = 30)
             self.camera.vflip = True
-            self.camera.resolution = (300, 280)
+            self.camera.resolution = (1024, 768)
             self.camera.start_preview()
             time.sleep(2)
+            self.stream = io.BytesIO()
         except:
-            pass
+            self.setup_was_successful = False
             
+    async def stream_data(self , connection):
+        try:
+            for i in self.camera.capture_continuous(self.stream, 'jpeg' , use_video_port =True):
+                await connection.send(self.stream.read())
+                self.stream.seek(0)
+                self.stream.truncate()
+                await asyncio.sleep(0.1)
+        except:
+            pass # reenable the voice commands
+
+
